bf3780548f766e15a6939f7c508107ff83e0fb29
Dorcas Mucheche <dorcasmucheche@gmail.com>
2026-01-26 07:02:19 +0200

    feat: create database service

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,10 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
- export const CONFIG = {
-   PORT:   process.env.PORT || 4000
-    
-    }
+export const CONFIG = {
+  PORT: process.env.PORT || 4000,
+  MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
+  MONGODB_USERNAME: process.env.MONGODB_USERNAME,
+  MONGODB_HOST: process.env.MONGODB_HOST,
+};
