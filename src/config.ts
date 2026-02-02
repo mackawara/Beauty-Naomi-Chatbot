287fbec8dc8fa1e820ad9b4f6f61c697e6980afa
hxscyprodz <hxscyprodz@gmail.com>
2026-02-02 14:18:19 +0200

    Add configuration rules

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,50 @@
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+import { logger } from "./services/logger";
+dotenv.config({ path: path.join(__dirname, "../.env") });
+
+const TAG = "CONFIG";
+
+const mandatoryEnvironmentConstants = [
+  "APP_ENV",
+  "PORT",
+  "MONGODB_PASSWORD",
+  "MONGODB_USERNAME",
+  "MONGODB_HOST",
+];
+
+const missingEnvironmentVariables = mandatoryEnvironmentConstants.filter(
+  (constant) => !process.env[constant],
+);
+
+if (missingEnvironmentVariables.length > 0) {
+  const constantsString = JSON.stringify(missingEnvironmentVariables);
+
+  logger.info(
+    `[${TAG}] Environment variable(s) ${constantsString.substring(
+      1,
+      constantsString.length - 1,
+    )} required. If running on local server, create a .env file in the root folder and define them in that file like: 
+      
+  MONGODB_USERNAME=username
+  MONGODB_PASSWORD=password
+  MONGODB_DATABASE_HOST=cluster_path/database_name
+  ...
+  `,
+  );
+
+  process.exit(1);
+}
 
 export const CONFIG = {
-  PORT: process.env.PORT || 4000,
-  MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
-  MONGODB_USERNAME: process.env.MONGODB_USERNAME,
-  MONGODB_HOST: process.env.MONGODB_HOST,
-};
+  IS_LOCAL_ENVIRONMENT: process.env.APP_ENV !== "production",
+  PORT: parseInt(process.env.PORT || "0", 10) || 4000,
+  MONGODB_HOST: process.env.MONGODB_HOST || "0.0.0.0",
+  MONGODB_PASSWORD: process.env.MONGODB_PASSWORD || "",
+  MONGODB_USERNAME: process.env.MONGODB_USERNAME || "",
+
+}; 
+logger.warn(
+  `[${TAG}] Running in ${CONFIG.IS_LOCAL_ENVIRONMENT ? "LOCAL" : "PRODUCTION"} environment`,
+);
