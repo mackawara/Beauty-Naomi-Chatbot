f011e9664d00296867f8b37de6a1895950e20491
Dorcas Mucheche <dorcasmucheche@gmail.com>
2026-03-16 14:09:43 +0200

    feat: create appointments model

---
+++ src/models/Appointments.ts
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+
+// Item Details Sub-schema
+const bookingItemSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+    default: 1
+  },
+  priceAtOrder: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  productRetailerId: {
+    type: String,
+    required: true
+  },
+  unitPrice: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  subtotal: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+}, { _id: true }); 
+
+
+const bookingSchema = new mongoose.Schema({
+  bookingId: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  
+  // Array of items 
+  items: [bookingItemSchema],
+  
+  totalAmount: {
+    type: Number,
+    default: 0,
+    min: 0
+  },
+  
+  serviceName: {
+    type: String,
+  },
+  
+  bookingDate: {
+    type: Date,
+    default: null
+  },
+  appointmentTime: {
+    type: String,
+    default: ""
+  },
+  notes: {
+    type: String,
+    default: "Order received via WhatsApp"
+  },
+  },
+ {
+  timestamps: true
+});
+
+
+export const Appointment = mongoose.model('Appointment', bookingSchema);
